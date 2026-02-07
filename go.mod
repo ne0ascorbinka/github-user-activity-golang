7ffdbae33de611e4297fd4eee1cb1180b4971355
ne0ascorbinka <ne.ascorbinka@gmail.com>
2026-02-07 18:24:42 +0200

    Add initial implementation for GitHub user activity tracking

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ne0ascorbinka/github-user-activity-golang
+
+go 1.25.6
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type EventType string
+
+const (
+	EVENT_TYPE_CREATE EventType = "CreateEvent"
+	EVENT_TYPE_PUSH   EventType = "PushEvent"
+)
+
+type Event struct{
+	// ID int `json:"id"`
+	Type EventType `json:"type"`
+	Repo struct{
+		Name string `json:"name"`
+	} `json:"repo"`
+}
+
+type Events []Event
+
+func (e Event) ProcessPushEvent() {
+	fmt.Printf("Pushed to %s\n", e.Repo.Name)
+}
+
+func (e Event) ProcessEvent() {
+	switch e.Type {
+	case EVENT_TYPE_PUSH:
+		e.ProcessPushEvent()
+	default:
+		fmt.Printf("Skipping unknown event of type %s\n", e.Type)
+	}
+}
+
+const url = "https://api.github.com/users/%s/events"
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Script takes username as argument")
+		os.Exit(1)
+	}
+	username := os.Args[1]
+
+	r, err := http.Get(fmt.Sprintf(url, username))
+	if err != nil {
+		log.Fatalf("Error fetching GitHub data: %v", err)
+	}
+	defer r.Body.Close()
+
+	var events Events
+	err = json.NewDecoder(r.Body).Decode(&events)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Bad JSON: %s\n", err)
+		os.Exit(1)
+	}
+
+	for _, event := range events {
+		event.ProcessEvent()
+	}
+}
